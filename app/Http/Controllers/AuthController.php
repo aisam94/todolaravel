564e0bc4e3a3d7601f889b565f198f26623cf736
Aisamuddin <aisam0094@gmail.com>
2022-11-23 13:30:29 +0800

    added authentication functionalities

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session as FacadesSession;
+
+class AuthController extends Controller
+{
+    //
+    public function login()
+    {
+        // show login view
+        // if already sign in, redirect to dashboard
+        if (Auth::check()) {
+            return redirect('dashboard');
+        }
+        return view('auth.login');
+    }
+
+    public function customLogin(Request $request)
+    {
+        // validate request first
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        // set credentials
+        $credentials = $request->only('email', 'password');
+        // if successfull, redirect to dashboard
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')->withSuccess('Signed in');
+        }
+        // if fail auth
+        return redirect('login')->withSuccess('Login details are not valid');
+    }
+
+
+    public function register()
+    {
+        // if already sign in, redirect to dashboard
+        if (Auth::check()) {
+            return redirect('dashboard');
+        }
+        return view('auth.register');
+    }
+
+    public function customRegister(Request $request)
+    {
+        // validate request data first
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        // get all data regarding user input and create user
+        // using this controller function create
+        $data = $request->all();
+        $check = $this->create($data);
+
+
+        // set credentials
+        $credentials = $request->only('email', 'password');
+        // if successfull, redirect to dashboard
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')->withSuccess('Signed in');
+        }
+
+        // redirect to login page if somehow fail login after registering
+        return redirect('login');
+    }
+
+    public function create(array $data)
+    {
+        return User::create(
+            [
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]
+        );
+    }
+
+    public function dashboard()
+    {
+        // if pass auth, return dashboard view
+        // if not redirect to login with message
+        if (Auth::check()) {
+            return view('dashboard');
+        }
+        return redirect("login")->withSuccess('You are not given access');
+    }
+
+    public function signOut()
+    {
+        // sign out
+        // Session::flush();
+        FacadesSession::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +20,25 @@ use Illuminate\Support\Facades\Route;
 //     // view argument based on views folder file name
 // });
 
+// Todo routes
 Route::get('/', [TodoController::class, 'index']);
 Route::get('create', [TodoController::class, 'create']);
 Route::get('details/{todo}', [TodoController::class, 'details']);
 Route::get('edit/{todo}', [TodoController::class, 'edit']);
 Route::post('update/{todo}', [TodoController::class, 'update']);
 Route::get('delete/{todo}', [TodoController::class, 'delete']);
+
+// Auth routes
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
+
+Route::get('register', [AuthController::class, 'register'])->name('register-user');
+Route::post('custom-register', [AuthController::class, 'customRegister'])->name('register.custom');
+
+Route::get('signout', [AuthController::class, 'signOut'])->name('signout');
+
 
 // sending data to server
 Route::post('store-data', [TodoController::class, 'store']);
